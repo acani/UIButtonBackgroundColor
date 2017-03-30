e1a944fc6ece877f52b38e92e9247b876b1890b2
Wes Van der Klomp <wes.vdk@icloud.com>
2017-03-30 10:30:12 -0700

    fix tests for swift 3

--- UIButtonBackgroundColorTests/UIButtonBackgroundColorTests.swift
+++ UIButtonBackgroundColorTests/UIButtonBackgroundColorTests.swift
@@ -5,15 +5,17 @@ class UIButtonBackgroundColorTests: XCTestCase {
     func testSetBackgroundColor() {
         let button = UIButton(type: .custom)
         XCTAssertNil(button.backgroundImage(for: UIControlState()))
-        button.setBackgroundColor
+        
+        
+        button.setBackgroundColor(.blue, forState: .normal)
         XCTAssertNotNil(button.backgroundImage(for: UIControlState()))
-        button.setBackgroundColor(nil, forState: .Normal)
+        button.setBackgroundColor(nil, forState: .normal)
         XCTAssertNil(button.backgroundImage(for: UIControlState()))
     }
 
     func testImageColored() {
-        let image = UIImage.imageColored(.blueColor())
-        let pixelData = (image.CGImage).dataProvider.data
+        let image = UIImage.imageColored(.blue)
+        let pixelData = (image?.cgImage)?.dataProvider?.data
         let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
         // Assert that alpha (0) & blue (3) are both 255 and the rest are 0
         for index in 0..<CFDataGetLength(pixelData) {
